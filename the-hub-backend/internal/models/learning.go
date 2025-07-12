8b1ab2ea035e1ebf55746cfd478b7336cf5393dc
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-07-12 23:26:48 +0200

    Fixes to learning model

--- the-hub-backend/internal/models/learning.go
+++ the-hub-backend/internal/models/learning.go
@@ -9,11 +9,21 @@ import (
 type Deck struct {
 	ID        uint           `json:"deck_id" gorm:"primaryKey"`
 	Name      string         `json:"name" gorm:"not null"`
-	UserID    uint           `json:"user_id"`
-	Cards     []Card         `json:"cards" gorm:"foreignKey:DeckID"`
+	UserID    uint           `json:"user_id" gorm:"not null"`
+	Cards     []Card         `json:"-"`
+	User      User           `json:"-" gorm:"foreignKey:UserID"`
 	CreatedAt time.Time      `json:"-"`
 	UpdatedAt time.Time      `json:"-"`
-	DeleteAt  gorm.DeletedAt `json:"-" gorm:"index"`
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+}
+
+type DeckUser struct {
+	ID     uint `gorm:"primaryKey"`
+	DeckID uint
+	UserID uint
+	Role   string // "owner", "editor", "viewer"
+	Deck   Deck   `gorm:"foreignKey:DeckID"`
+	User   User   `gorm:"foreignKey:UserID"`
 }
 
 type Card struct {
@@ -21,12 +31,13 @@ type Card struct {
 	DeckID       uint           `json:"deck_id" gorm:"not null"`
 	Question     string         `json:"question" gorm:"not null"`
 	Answer       string         `json:"answer" gorm:"not null"`
-	Easiness     float64        `json:"-" gorm:"default:2.5"` // SM-2 easiness factor
-	Interval     int            `json:"-" gorm:"default:1"`   // Days until next review
-	Repetitions  int            `json:"-" gorm:"default:0"`   // Successful reviews in a row
-	LastReviewed time.Time      `json:"-"`                    // Last time card was reviewed
-	NextReview   time.Time      `json:"-"`                    // When the card should next appear
+	Easiness     float64        `json:"-" gorm:"default:2.5"`     // SM-2 easiness factor
+	Interval     int            `json:"-" gorm:"default:1"`       // Days until next review
+	Repetitions  int            `json:"-" gorm:"default:0"`       // Successful reviews in a row
+	LastReviewed time.Time      `json:"last_review"`              // Last time card was reviewed
+	NextReview   time.Time      `json:"next_review" gorm:"index"` // When the card should next appear
+	Deck         Deck           `json:"-" gorm:"foreignKey:DeckID"`
 	CreatedAt    time.Time      `json:"-"`
 	UpdatedAt    time.Time      `json:"-"`
-	DeleteAt     gorm.DeletedAt `json:"-" gorm:"index"`
+	DeletedAt    gorm.DeletedAt `json:"-" gorm:"index"`
 }
