d5a95675c122ae2560bc1aed2b589aeb7739638f
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-07-13 21:35:13 +0200

    Removed on delete contraint
    Had added by decided to remove to keep tasks and scheduled tasks
    separate

--- the-hub-backend/internal/models/scheduled_task.go
+++ the-hub-backend/internal/models/scheduled_task.go
@@ -6,12 +6,12 @@ import "time"
 type ScheduledTask struct {
 	ID          uint      `json:"id" gorm:"primaryKey"`
 	TaskID      uint      `json:"task_id" gorm:"not null;uniqueIndex"`
-	Task        Task      `json:"tasks" gorm:"constraint:OnDelete:CASCADE;"`
+	Task        Task      `json:"-"`
 	Title       string    `json:"title" gorm:"not null"`
 	Start       time.Time `json:"start" gorm:"not null"`
 	End         time.Time `json:"end" gorm:"not null"`
 	UserID      uint      `json:"user_id"`
-	User        User      `gorm:"foreignKey:UserID"`
+	User        User      `json:"-" gorm:"foreignKey:UserID"`
 	CreatedByAI bool      `json:"created_by_ai" gorm:"default:false"`
 	CreateAt    time.Time `json:"-"`
 	UpdatedAt   time.Time `json:"-"`
