1a7a2c127e0120b5c22c002902bb6bfc1aed0468
TheoMKgosi <theo.kgosiemang12@gmail.com>
2025-07-27 21:49:16 +0200

    Fix and refactor: AllowMethods has proper strings and relocated routes

---
+++ the-hub-backend/internal/routes/routes.go
@@ -0,0 +1,93 @@
+package routes
+
+import (
+	"github.com/TheoMKgosi/The-hub/internal/handlers"
+	"github.com/TheoMKgosi/The-hub/internal/util"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func RegisterRoutes(router *gin.Engine) {
+	// Public routes
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	})
+
+	router.POST("/register", handlers.Register)
+	router.POST("/login", handlers.Login)
+
+	protected := router.Group("/")
+	protected.Use(util.JWTAuthMiddleware())
+
+	// Plan routes
+	// -- Goal routes
+	protected.GET("/goals", handlers.GetGoals)
+	protected.GET("/goals/:ID", handlers.GetGoal)
+	protected.POST("/goals", handlers.CreateGoal)
+	protected.PUT("/goals/:ID", handlers.UpdateGoal)
+	protected.DELETE("/goals/:ID", handlers.DeleteGoal)
+
+	// -- Task routes
+	protected.GET("/tasks", handlers.GetTasks)
+	protected.GET("/tasks/:ID", handlers.GetTask)
+	protected.POST("/tasks", handlers.CreateTask)
+	protected.PATCH("/tasks/:ID", handlers.UpdateTask)
+	protected.DELETE("/tasks/:ID", handlers.DeleteTask)
+
+	// Time routes
+	// -- Schedule routes
+	protected.GET("/schedule", handlers.GetSchedule)
+	protected.POST("/schedule", handlers.CreateSchedule)
+	protected.DELETE("/schedule/:ID", handlers.DeleteSchedule)
+
+	// Learning routes
+	// -- Deck routes
+	protected.GET("/decks", handlers.GetDecks)
+	protected.GET("/decks/:ID", handlers.GetDeck)
+	protected.POST("/decks", handlers.CreateDeck)
+	protected.PATCH("/decks/:ID", handlers.UpdateDeck)
+	protected.DELETE("/decks/:ID", handlers.DeleteDeck)
+
+	// -- Card routes
+	protected.GET("/decks/cards/:deckID", handlers.GetCards)
+	protected.GET("/cards/:ID", handlers.GetCard)
+	protected.POST("/cards", handlers.CreateCard)
+	protected.PATCH("/cards/:ID", handlers.UpdateCard)
+	protected.DELETE("/cards/:ID", handlers.DeleteCard)
+
+	protected.GET("/cards/review/:ID", handlers.ReviewCard)
+	protected.GET("/cards/due/:deckID", handlers.GetDueCards)
+
+	// -- Topic routes
+	protected.GET("/topics", handlers.GetTopics)
+	protected.POST("/topics", handlers.CreateTopic)
+	protected.PATCH("/topics/:ID", handlers.UpdateTopic)
+	protected.DELETE("/topics/:ID", handlers.DeleteTopic)
+
+
+	// -- Tag routes
+	protected.GET("/tags", handlers.GetTags)
+	protected.POST("/tags", handlers.CreateTag)
+	protected.PATCH("/tags/:ID", handlers.UpdateTag)
+	protected.DELETE("/tags/:ID", handlers.DeleteTag)
+
+	// Finance routes
+	// -- Category routes
+	protected.GET("/categories", handlers.GetBudgetCategories)
+	protected.POST("/categories", handlers.CreateBudgetCategory)
+	protected.PATCH("/categories/:ID", handlers.UpdateBudgetCategory)
+	protected.DELETE("/categories/:ID", handlers.DeleteBudgetCategory)
+
+	// -- Budget routes
+	protected.GET("/budgets", handlers.GetBudgets)
+	protected.POST("/budgets", handlers.CreateBudget)
+	protected.PATCH("/budgets/:ID", handlers.UpdateBudget)
+	protected.DELETE("/budgets/:ID", handlers.DeleteBudget)
+
+	// -- Income routes
+	protected.GET("/incomes", handlers.GetIncomes)
+	protected.POST("/incomes", handlers.CreateIncome)
+	protected.PATCH("/incomes/:ID", handlers.UpdateIncome)
+	protected.DELETE("/incomes/:ID", handlers.DeleteIncome)
+}
+
--- the-hub-backend/main.go
+++ the-hub-backend/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/TheoMKgosi/The-hub/internal/config"
-	"github.com/TheoMKgosi/The-hub/internal/handlers"
-	"github.com/TheoMKgosi/The-hub/internal/util"
+	"github.com/TheoMKgosi/The-hub/internal/routes"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -19,6 +17,9 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	config.InitLogger()
+	defer config.Logger.Sync()
 
 	config.InitDBSQLite()
 
@@ -31,79 +32,14 @@ func main() {
 
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{os.Getenv("ALLOWED_URL")},
-		AllowMethods:     []string{os.Getenv("ALLOWED_URL")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
-	// Testing route
-	router.GET("/ping", ping)
 
-	// User routes
-	router.POST("/register", handlers.Register)
-	router.POST("/login", handlers.Login)
-
-	protected := router.Group("/")
-	protected.Use(util.JWTAuthMiddleware())
-
-	// Goal routes
-	protected.GET("/goals", handlers.GetGoals)
-	protected.GET("/goals/:ID", handlers.GetGoal)
-	protected.POST("/goals", handlers.CreateGoal)
-	protected.PUT("/goals/:ID", handlers.UpdateGoal)
-	protected.DELETE("/goals/:ID", handlers.DeleteGoal)
-
-	// Task routes
-	protected.GET("/tasks", handlers.GetTasks)
-	protected.GET("/tasks/:ID", handlers.GetTask)
-	protected.POST("/tasks", handlers.CreateTask)
-	protected.PATCH("/tasks/:ID", handlers.UpdateTask)
-	protected.DELETE("/tasks/:ID", handlers.DeleteTask)
-
-	// Schedule routes
-	protected.GET("/schedule", handlers.GetSchedule)
-	protected.POST("/schedule", handlers.CreateSchedule)
-	protected.DELETE("/schedule/:ID", handlers.DeleteSchedule)
-
-	// Learning routes
-	protected.GET("/decks", handlers.GetDecks)
-	protected.GET("/decks/:ID", handlers.GetDeck)
-	protected.POST("/decks", handlers.CreateDeck)
-	protected.PATCH("/decks/:ID", handlers.UpdateDeck)
-	protected.DELETE("/decks/:ID", handlers.DeleteDeck)
-
-	protected.GET("decks/cards/:deckID", handlers.GetCards)
-	protected.GET("/cards/:ID", handlers.GetCard)
-	protected.POST("/cards", handlers.CreateCard)
-	protected.PATCH("/cards/:ID", handlers.UpdateCard)
-	protected.DELETE("/cards/:ID", handlers.DeleteCard)
-
-	protected.GET("/cards/review/:ID", handlers.ReviewCard)
-	protected.GET("/cards/due/:deckID", handlers.GetDueCards)
-
-	// Finance routes 
-	protected.GET("/categories", handlers.GetBudgetCategories)
-	protected.POST("/categories", handlers.CreateBudgetCategory)
-	protected.PATCH("/categories/:ID", handlers.UpdateBudgetCategory)
-	protected.DELETE("/categories/:ID", handlers.DeleteBudgetCategory)
-
-	protected.GET("/budgets", handlers.GetBudgets)
-	protected.POST("/budgets", handlers.CreateBudget)
-	protected.PATCH("/budgets/:ID", handlers.UpdateBudget)
-	protected.DELETE("/budgets/:ID", handlers.DeleteBudget)
-
-
-	protected.GET("/incomes", handlers.GetIncomes)
-	protected.POST("/incomes", handlers.CreateIncome)
-	protected.PATCH("/incomes/:ID", handlers.UpdateIncome)
-	protected.DELETE("/incomes/:ID", handlers.DeleteIncome)
+	routes.RegisterRoutes(router)
 
 	router.Run(os.Getenv("PORT"))
-}
-
-func ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
 }
