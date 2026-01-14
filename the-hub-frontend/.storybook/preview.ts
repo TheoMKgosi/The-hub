db7e13be73fe402edf99b973fdd79d9cae10cbd0
TheoMKgosi <theo.kgosiemang12@gmail.com>
2026-01-14 08:31:54 +0200

    Storiesjs config file changes

--- the-hub-frontend/.storybook/preview.ts
+++ the-hub-frontend/.storybook/preview.ts
@@ -1,12 +1,22 @@
-import type { Preview } from '@storybook-vue/nuxt'
+import { setup, type Preview } from '@storybook-vue/nuxt'
 import '../app/assets/css/main.css'
+import { createPinia } from 'pinia';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+dayjs.extend(relativeTime);
+
+setup((app) => {
+  app.use(createPinia())
+}
+)
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
