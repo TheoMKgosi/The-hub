681996669fd252e137a2a7f2ca8f18fe715917b7
TheoMKgosi <theo.kgosiemang12@gmail.com>
2026-01-30 01:39:29 +0200

    wip: refactor the learning section

---
+++ the-hub-frontend/app/components/ui/svg/BoltIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <svg class="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+    </svg>
+  </div>
+</template>
---
+++ the-hub-frontend/app/components/ui/svg/CheckMarkIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+    </svg>
+  </div>
+</template>
