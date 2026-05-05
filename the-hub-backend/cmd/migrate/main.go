32773f8d84f800a36eb7b57205d101099c95d0fd
TheoMKgosi <theo.kgosiemang12@gmail.com>
2026-05-05 18:51:49 +0200

    config: Docker for development

--- the-hub-backend/cmd/migrate/main.go
+++ the-hub-backend/cmd/migrate/main.go
@@ -13,10 +13,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: migrate <command> [args...]")
